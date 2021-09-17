2ff315c4212b55718dd2b32628f1e86ba31fbd05
sajad-lp16 <sajad.tohidi76@gmail.com>
2021-09-17 15:40:51 +0430

    define user account modification feature and register it url

--- accounts/api/urls.py
+++ accounts/api/urls.py
@@ -6,4 +6,7 @@ app_name = 'accounts_api'
 
 urlpatterns = [
     path('list-create/', views.UserListCreate.as_view(), name='list_create'),
+
+    path('retrieve-update-destroy/<str:username>/',
+         views.UserRetrieveUpdateDestroy.as_view(), name='retrieve_update_destroy'),
 ]
--- accounts/api/views.py
+++ accounts/api/views.py
@@ -4,6 +4,7 @@ from rest_framework import generics
 from rest_framework.permissions import AllowAny
 
 from . import serializers
+from ..utils.permissions import AccountOwnerPermission
 
 User = get_user_model()
 
@@ -12,3 +13,11 @@ class UserListCreate(generics.ListCreateAPIView):
     serializer_class = serializers.UserSerializer
     queryset = User.objects.all()
     permission_classes = AllowAny,
+
+
+class UserRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = serializers.UserSerializer
+    queryset = User.objects.all()
+    lookup_url_kwarg = 'username'
+    lookup_field = 'username'
+    permission_classes = AccountOwnerPermission,
