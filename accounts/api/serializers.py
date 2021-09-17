986659a29501200965bdda26215cb4a7d32b5a5d
sajad-lp16 <sajad.tohidi76@gmail.com>
2021-09-17 22:37:07 +0430

    preview active token on get requests (each user can see his token only)

--- accounts/api/serializers.py
+++ accounts/api/serializers.py
@@ -5,7 +5,6 @@ from django.contrib.auth import get_user_model
 from django.contrib.auth.hashers import make_password
 from rest_framework import serializers
 
-
 User = get_user_model()
 
 CLIENT_ID = '03pUssR1v7l5BdTcDQkgmN9jVs70nMU5AO7c2AGE'
@@ -27,12 +26,14 @@ class UserSerializer(serializers.ModelSerializer):
             'is_active',
             'is_staff',
             'is_private',
+            'is_verified',
             'avatar',
         )
 
         extra_kwargs = {
             'is_staff': {'read_only': True},
             'is_active': {'read_only': True},
+            'is_verified': {'read_only': True},
             'password': {'write_only': True},
         }
 
@@ -54,3 +55,13 @@ class UserSerializer(serializers.ModelSerializer):
         if password is not None:
             validated_data['password'] = make_password(password)
         return super().update(instance, validated_data)
+
+    def to_representation(self, instance):
+        request = self.context['request']
+        data = super().to_representation(instance)
+        if request.user == instance or request.user.is_superuser:
+            data['token'] = instance.oauth2_provider_accesstoken.get_queryset()[0].token
+        else:
+            data.pop('phone_number')
+            data.pop('email')
+        return data
