38194ef18d571646aa61177bab0d8b03f9d18845
sajad-lp16 <sajad.tohidi76@gmail.com>
2021-09-17 22:58:29 +0430

    avoid self following and also add verbose name for relations app

--- relations/apps.py
+++ relations/apps.py
@@ -1,6 +1,11 @@
+from django.utils.translation import ugettext_lazy as _
 from django.apps import AppConfig
 
 
 class RelationsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'relations'
+
+    verbose_name = _('relation')
+    verbose_name_plural = _('relations')
+
--- relations/models.py
+++ relations/models.py
@@ -1,6 +1,7 @@
 from django.utils.translation import ugettext_lazy as _
 from django.contrib.auth import get_user_model
 from django.db import models
+from rest_framework.exceptions import ValidationError
 
 from utils.models import BaseModel
 
@@ -19,3 +20,12 @@ class Relation(BaseModel):
 
     def __str__(self):
         return f'{self.start_user} -> {self.end_user}'
+
+    def clean(self):
+        if self.start_user == self.end_user:
+            raise ValidationError('Users cant be the same!')
+
+    def save(self, force_insert=False, force_update=False, using=None,
+             update_fields=None):
+        self.full_clean()
+        return super().save(force_insert=False, force_update=False, using=None, update_fields=None)
