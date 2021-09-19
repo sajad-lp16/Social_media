43f0e7634b88eb73d7c8d371c26f95e79674e5cf
sajad-lp16 <sajad.tohidi76@gmail.com>
2021-09-19 13:22:13 +0430

    make media filed optional and add manager for conversation model on chat application

--- chat/models.py
+++ chat/models.py
@@ -1,3 +1,5 @@
+from uuid import uuid4
+
 from django.contrib.auth import get_user_model
 from django.core.validators import FileExtensionValidator
 from django.utils.translation import ugettext_lazy as _
@@ -9,10 +11,20 @@ from .utils import functions
 User = get_user_model()
 
 
+class ConversationManager(models.Manager):
+    def get_conversation(self, user_id):
+        return self.filter(
+            models.Q(start_user_id=user_id) | models.Q(end_user_id=user_id)
+        ).distinct()
+
+
 class Conversation(BaseModel):
-    start_user = models.ForeignKey(User, related_name='chat_start', on_delete=models.CASCADE, verbose_name=_('user'))
-    end_user = models.ForeignKey(User, related_name='chat_end', on_delete=models.CASCADE, verbose_name=_('user'))
+    start_user = models.ForeignKey(User, related_name='chat_start', on_delete=models.CASCADE,
+                                   verbose_name=_('start user'))
+    end_user = models.ForeignKey(User, related_name='chat_end', on_delete=models.CASCADE, verbose_name=_('end user'))
     slug = models.SlugField(max_length=150, unique=True, verbose_name=_('slug'), blank=True)
+
+    objects = ConversationManager()
 
     def __str__(self):
         return f'{self.start_user.__str__()} and {self.end_user.__str__()}'
@@ -30,8 +42,9 @@ class Conversation(BaseModel):
 
 
 class Message(BaseModel):
+    id = models.UUIDField(primary_key=True, default=uuid4)
     user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name=_('user'), related_name='messages')
-    media = models.FileField(verbose_name=_('media'), upload_to=functions.rename_file,
+    media = models.FileField(verbose_name=_('media'), upload_to=functions.rename_file, blank=True, null=True,
                              validators=[FileExtensionValidator(
                                  allowed_extensions=['jpg', 'mkv', 'mp4', 'flv', 'avi', 'png', 'jpeg'])])
 
@@ -39,8 +52,13 @@ class Message(BaseModel):
     conversation = models.ForeignKey(Conversation, on_delete=models.CASCADE,
                                      verbose_name=_('conversation'), related_name='messages')
 
+    def get_target_user(self):
+        if self.user == self.conversation.start_user:
+            return self.conversation.end_user
+        return self.conversation.start_user
+
     def __str__(self):
-        return f'{self.user.__str__()} to {self.conversation.end_user.__str__()}'
+        return f'{self.user.__str__()} to {self.get_target_user().__str__()}'
 
     class Meta:
         db_table = 'messages'
