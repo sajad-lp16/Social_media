50b66579ccc5a48d4f8eee9fb6dec7391862b6eb
sajad-lp16 <sajad.tohidi76@gmail.com>
2021-09-19 23:12:21 +0430

    provide a manager for post model

--- social/models.py
+++ social/models.py
@@ -9,10 +9,22 @@ from .utils import functions
 User = get_user_model()
 
 
+class PostManager(models.Manager):
+    def get_followings_posts(self, user):
+        followings_posts = self.none()
+        followings_relations = user.followings.select_related('end_user').all()
+        followings = [followings_relation.end_user for followings_relation in followings_relations]
+        for following in followings:
+            followings_posts = followings_posts.union(following.posts.all())
+        return followings_posts
+
+
 class Post(BaseModel):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts', verbose_name=_('user'))
     caption = models.CharField(_('caption'), max_length=300, blank=True)
     slug = models.SlugField(_('slug'), max_length=150, blank=True, unique=True)
+
+    objects = PostManager()
 
     def __str__(self):
         return self.user.__str__()
