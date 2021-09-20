dab4d3e5ea3d984cdff297045dfddad7ed337087
sajad-lp16 <sajad.tohidi76@gmail.com>
2021-09-20 17:44:28 +0430

    add relation serializer

--- relations/api/serializers.py
+++ relations/api/serializers.py
@@ -1,3 +1,22 @@
+from django.contrib.auth import get_user_model
+
 from rest_framework import serializers
+from accounts.api.serializers import UserSerializer
+
+User = get_user_model()
 
 from .. import models
+
+
+class RelationSerializer(serializers.ModelSerializer):
+    start_user = serializers.StringRelatedField(read_only=True)
+    end_user = serializers.StringRelatedField(read_only=True)
+    target = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = models.Relation
+        fields = (
+            'start_user',
+            'end_user',
+            'target'
+        )
